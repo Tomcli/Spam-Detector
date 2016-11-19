da7b55ae74262afde5c4245494c148f46634c391
Tommy Li <dfplayer95@yahoo.com>
2016-11-18 22:00:23 -0800

    added data

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from IPython.display import display # Allows the use of display() for DataFrames
+import matplotlib.pyplot as plt
+import matplotlib
+import data as stock
+import learner
+from parse import *
+from datetime import datetime
+
+def run():
+	print 'Finish detection'
+
+if __name__ == '__main__':
+    run()
