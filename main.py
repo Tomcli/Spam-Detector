5b0ac80419e0fc4ec2b32440b300352279376dfa
Tommy Li <dfplayer95@yahoo.com>
2016-11-22 22:30:11 -0800

    added simple dataframe to catagorize words

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ text_spam = {}
 
 def run():
 	for root, dirs, files in os.walk("enron1/ham/"):
-		for name in files:
+		for i, name in enumerate(files):
+			if i == 1500:
+				break
 			path = os.path.join(root, name)
 			if os.path.isfile(path):
 				txt = open(path,"r")
@@ -27,7 +29,9 @@ def run():
 				txt.close()
 		print 'Number of hams:', len(files)
 	for root, dirs, files in os.walk("enron1/spam/"):
-		for name in files:
+		for i, name in enumerate(files):
+			if i == 1500:
+				break
 			path = os.path.join(root, name)
 			if os.path.isfile(path):
 				txt = open(path,"r")
@@ -42,6 +46,15 @@ def run():
 		print 'Number of spams:', len(files)
 	# print text_ham
 	# print text_spam
+	ham_frame = pd.DataFrame(text_ham.items(), columns=['Word', 'H_Frequency'])
+	#ham_frame['ham'] = pd.Series(1, index=ham_frame.index)
+	spam_frame = pd.DataFrame(text_spam.items(), columns=['Word', 'S_Frequency'])
+	#spam_frame['ham'] = pd.Series(0, index=spam_frame.index)
+	data = pd.merge(ham_frame,spam_frame, on='Word')
+	data = data.loc[data['Word'].str.isalpha()]
+	data = data.loc[data['Word'].str.len() > 1] 
+	new_data = data.loc[(data['H_Frequency'] >= 300) ^ (data['S_Frequency'] >= 300)]
+	print new_data
 	print 'Finish detection'
 
 if __name__ == '__main__':
